52a6dc54c62cdcd3a742c0369dddbdf04bc35eb1
vinifred1003 <viniciusfrederico1003@gmail.com>
2024-03-25 08:16:54 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { categories } from './api';
+import Row from './components/Row'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        {/*Navbar*/}
+        {/*Banner*/}
+        {/*Categorias - Linhas*/}
+
+        {categories.map( (category) => {
+
+            return  <Row 
+                         key={category.name}
+                         title={category.title} 
+                         isLarge={category.isLarge}
+                         path={category.path}
+                    />
+
+        })}
+
     </div>
   );
 }
